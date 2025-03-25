d6c3836a5b33953d7bfc878badd2f969833882a1
yanira2876 <162449857+yanira2876@users.noreply.github.com>
2025-03-25 12:35:49 -0300

    Add files via upload

---
+++ monedas.py
@@ -0,0 +1,11 @@
+#yanira cayo
+#holi
+#25/03/2025
+
+
+pesos = int(input("cuanto te queda en pesasos colombianos?"))
+soles = int(input("cuanto te queda en pesasos soles peruanos?"))
+reales = int(input("cuanto te queda en pesasos reales brazil ?"))
+
+total = pesos * 0.00025 + soles * 0.28 + reales * 0.21 
+print(total)
